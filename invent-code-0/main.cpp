7b2271fcf6846f018729c0c635f5aa1c5d10fb5a
anaygupta2004 <55030203+anaygupta2004@users.noreply.github.com>
2020-11-15 18:22:33 +0000

    Saving progress for all questions.

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -2,6 +2,26 @@
 #include <string>
 using namespace std;
 
+struct Point {
+  int x;
+  int y;
+};
+
+void PointStruct (val) {
+  cout << "x-value: ";
+  cin >> val.x;
+  cout << val.x;
+  cout << "y-value: ";
+  cin >> val.y;
+}
+
+void PointStructPrint (val) {
+  cout << "Final Coordinate: (" << val.x << ", " << val.y << ")" << endl;
+}
+
 int main() {
+  Point my_point;
+  PointStruct(my_point);
+  PointStructPrint(my_point);
   return 0;
 }
